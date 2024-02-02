d04616ecb8e085d60582486e48b32086275734d1
Romero <rrcb@cin.ufpe.br>
2024-02-02 20:05:28 -0300

    setup upp client and server

--- exercicio3/udp/client/main.go
+++ exercicio3/udp/client/main.go
@@ -1,1 +1,78 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
+	"net"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	// Resolve server address
+	//absolutePath, err := filepath.Abs("imgs/Apple.png")
+	absolutePath, err := filepath.Abs("imgs/Cake.png")
+	// absolutePath, err := filepath.Abs("imgs/Painting.png")
+	//absolutePath, err := filepath.Abs("imgs/Star.png")
+	if err != nil {
+		fmt.Println("Error getting absolute path: ", err)
+		return
+	}
+	img, err := openImage(absolutePath)
+	if err != nil {
+		fmt.Println("Error getting image: ", err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, img)
+	imageBytes := buf.Bytes()
+
+	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	// Create UDP connection
+	conn, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send a message to the server
+	_, err = conn.Write(imageBytes)
+	if err != nil {
+		fmt.Println("Error sending message:", err)
+		return
+	}
+
+	// Receive the response from the server
+	buffer := make([]byte, 1024)
+	n, _, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println("Error receiving response:", err)
+		return
+	}
+
+	fmt.Println("Server response:", string(buffer[:n]))
+}
+
+
+func openImage(path string) (image.Image, error) {
+	f, err := os.Open(path)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	img, _, err := image.Decode(f)
+
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
--- exercicio3/udp/server/main.go
+++ exercicio3/udp/server/main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"net"
+    "image"
+)
+
+func main() {
+	// Listen for incoming UDP packets on port 8080
+	serverAddr, err := net.ResolveUDPAddr("udp", ":8080")
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	// Create UDP listener
+	conn, err := net.ListenUDP("udp", serverAddr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Server listening on", serverAddr)
+
+	// Handle incoming UDP messages
+	for {
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn *net.UDPConn) {
+	buffer := make([]byte, 1024)
+
+	// Read from UDP connection
+	n, clientAddr, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println("Error reading from UDP:", err)
+		return
+	}
+
+    img := bytesToImg(conn)
+    fmt.Println(img)
+
+	// Echo the message back to the client
+	_, err = conn.WriteToUDP(buffer[:n], clientAddr)
+	if err != nil {
+		fmt.Println("Error writing to UDP:", err)
+		return
+	}
+}
+
+func bytesToImg(conn net.Conn) image.Image {
+	img, _, err := image.Decode(conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return img
+}
