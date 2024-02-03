29ba8eb336bfc1dcedb96f7cbd424f337fc79006
Romero <rrcb@cin.ufpe.br>
2024-02-03 00:09:01 -0300

    create udp implementation of greyscale

--- exercicio3/udp/client/main.go
+++ exercicio3/udp/client/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"image"
-	"image/png"
 	"net"
 	"os"
 	"path/filepath"
+    // "io"
 )
 
 func main() {
 	// Resolve server address
-	//absolutePath, err := filepath.Abs("imgs/Apple.png")
+	// absolutePath, err := filepath.Abs("imgs/Apple.png")
 	absolutePath, err := filepath.Abs("imgs/Cake.png")
 	// absolutePath, err := filepath.Abs("imgs/Painting.png")
 	//absolutePath, err := filepath.Abs("imgs/Star.png")
@@ -20,15 +19,7 @@ func main() {
 		fmt.Println("Error getting absolute path: ", err)
 		return
 	}
-	img, err := openImage(absolutePath)
-	if err != nil {
-		fmt.Println("Error getting image: ", err)
-		return
-	}
 
-	buf := new(bytes.Buffer)
-	err = png.Encode(buf, img)
-	imageBytes := buf.Bytes()
 
 	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8080")
 	if err != nil {
@@ -44,24 +35,19 @@ func main() {
 	}
 	defer conn.Close()
 
-	// Send a message to the server
-	_, err = conn.Write(imageBytes)
-	if err != nil {
-		fmt.Println("Error sending message:", err)
-		return
-	}
+    err = sendImage(conn, absolutePath)
+    if err != nil {
+        fmt.Println("Error sending image:", err)
+        return
+    }
 
-	// Receive the response from the server
-	buffer := make([]byte, 1024)
-	n, _, err := conn.ReadFromUDP(buffer)
-	if err != nil {
-		fmt.Println("Error receiving response:", err)
-		return
-	}
+    err = receiveImage(conn)
+    if err != nil {
+        fmt.Println("Error receiving image: ", err)
+        return
+    }
 
-	fmt.Println("Server response:", string(buffer[:n]))
 }
-
 
 func openImage(path string) (image.Image, error) {
 	f, err := os.Open(path)
@@ -75,4 +61,93 @@ func openImage(path string) (image.Image, error) {
 		return nil, err
 	}
 	return img, nil
+}
+
+func sendImage(conn *net.UDPConn, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+    bufferSize := 65000
+	buffer := make([]byte, bufferSize) // Use a larger buffer for handling potential larger chunks
+	reply := make([]byte, 1024) 
+	for {
+		// Read a chunk of the image file
+		n, err := file.Read(buffer)
+		if err != nil {
+            fmt.Println("Error reading from udp: ", err)
+            break
+    }
+        fmt.Println("Read bytes from buffer", n)
+
+		// Send the chunk to the server
+		_, err = conn.Write(buffer[:n])
+		if err != nil {
+			return err
+		}
+
+		if n < bufferSize {
+            fmt.Println("chegou no final")
+            break
+        }
+
+        nReply, _, err := conn.ReadFromUDP(reply);
+        if err != nil {
+            fmt.Println("Error reading server response: ", err)
+        }
+        fmt.Println(string(reply[:nReply]))
+
+	}
+
+	return nil
+}
+
+func receiveImage(conn *net.UDPConn) error {
+    bufferSize := 65000
+    buffer := make([]byte, bufferSize) // Use a larger buffer for handling potential larger chunks
+
+	// Receive the image data in chunks
+	var imageData []byte
+    i := 0
+	for {
+		n, _, err := conn.ReadFromUDP(buffer)
+        fmt.Println("N: ", n)
+        if err != nil {
+            fmt.Println("Error reading from udp: ", err)
+            break
+        }
+		imageData = append(imageData, buffer[:n]...)
+        if n < bufferSize {
+            fmt.Println("chegou no final")
+            break
+        }
+
+        i += 1
+		msg := fmt.Sprintf("Client response: Received chunk: %d", i)
+		_, err = conn.Write([]byte(msg))
+		if err != nil{
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Println("Recebeu tudo")
+	err := saveImage(imageData, "greyscale.png")
+	if err != nil{
+		fmt.Println(err)
+        return err
+	}
+    return nil
+}
+
+func saveImage(data []byte, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return err
 }
--- exercicio3/udp/server/main.go
+++ exercicio3/udp/server/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"net"
+    "io"
+	"os"
     "image"
+    "image/color"
+    "image/png"
+    "bytes"
+	"sync"
+	"runtime"
+    "path/filepath"
 )
-
 func main() {
 	// Listen for incoming UDP packets on port 8080
-	serverAddr, err := net.ResolveUDPAddr("udp", ":8080")
+	serverAddr, err := net.ResolveUDPAddr("udp", "localhost:8080")
 	if err != nil {
 		fmt.Println("Error resolving address:", err)
 		return
@@ -31,30 +38,237 @@ func main() {
 }
 
 func handleClient(conn *net.UDPConn) {
-	buffer := make([]byte, 1024)
+    bufferSize := 65000
+    buffer := make([]byte, bufferSize) // Use a larger buffer for handling potential larger chunks
 
-	// Read from UDP connection
-	n, clientAddr, err := conn.ReadFromUDP(buffer)
-	if err != nil {
-		fmt.Println("Error reading from UDP:", err)
-		return
+	// Receive the image data in chunks
+	var imageData []byte
+    var clientAddrFinal *net.UDPAddr
+    i := 0 
+	for {
+		fmt.Println("About to read buffer iteration: ", i)
+		n, clientAddr, err := conn.ReadFromUDP(buffer)
+        if err != nil {
+            fmt.Println("Error reading from udp: ", err)
+            break
+        }
+		fmt.Printf("Received %d bytes from %s\n", n, clientAddr)
+		imageData = append(imageData, buffer[:n]...)
+        if n < bufferSize {
+            fmt.Println("chegou no final")
+            clientAddrFinal = clientAddr;
+            break
+        }
+
+		i += 1
+		msg := fmt.Sprintf("Server response: Received chunk: %d", i)
+		_, err = conn.WriteToUDP([]byte(msg), clientAddr)
+		if err != nil{
+			fmt.Println(err)
+		}
+
 	}
 
-    img := bytesToImg(conn)
-    fmt.Println(img)
+	// Process the received image data (e.g., save it to a file)
+	img := bytesToImg(imageData)
+    pixels := imgToTensor(img)
+	greyScale(&pixels)
+	img = tensorToImg(pixels)
 
-	// Echo the message back to the client
-	_, err = conn.WriteToUDP(buffer[:n], clientAddr)
+    fmt.Println("Sending image...", clientAddrFinal)
+    
+	err := sendImage(conn, img, clientAddrFinal)
 	if err != nil {
-		fmt.Println("Error writing to UDP:", err)
-		return
+		fmt.Println("Error on send image: ", err)
 	}
+    fmt.Println("after Sending image")
+
+    // file, err := os.Create("greyscale.png")
+    // if err != nil {
+	// 	fmt.Println("Error creating file:", err)
+	// 	return
+	// }
+	// defer file.Close()
+	// err = png.Encode(file, img)
+    // if err !=nil {
+    //     fmt.Println("Error encoding png: ", err)
+    // }
+
 }
 
-func bytesToImg(conn net.Conn) image.Image {
-	img, _, err := image.Decode(conn)
+func saveImage(data []byte, filename string) error {
+	file, err := os.Create(filename)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	return img
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return err
 }
+
+func bytesToImg(imgBytes []byte) image.Image {
+ 	img, _, err := image.Decode(bytes.NewReader(imgBytes))
+ 	if err != nil {
+ 		fmt.Println("bytes to img error: ", err)
+ 	}
+ 	return img
+}
+
+func sendImage(conn *net.UDPConn, img image.Image, addr *net.UDPAddr) error {
+    fmt.Println("Started converting image to bytes")
+	imageBytes, err := imageToBytes(img)
+	if err != nil {
+		return err
+	}
+    err = saveImage(imageBytes, "temp.png")
+	if err != nil {
+        fmt.Println("Error creating temp file: ", err)
+        return err
+	}
+    
+    path, err := filepath.Abs("./temp.png")
+    if err != nil {
+        fmt.Println("Error getting filepath: ", err)
+        return err
+    }
+
+    tempFile, err := os.Open(path)
+    if err != nil {
+        fmt.Println("Error opening file: ", err)
+    }
+    defer tempFile.Close()
+    
+
+    _, err = tempFile.Write(imageBytes)
+    if err != nil {}
+
+
+	buffer := make([]byte, 65000) // Use a larger buffer for handling potential larger chunks
+	for {
+		// Read a chunk of the image file
+        fmt.Println("Inside for")
+		n, err := tempFile.Read(buffer)
+        fmt.Println("After read ", n)
+		if err != nil {
+			if err == io.EOF {
+                fmt.Println("EOF")
+				break // End of file, exit the loop
+			}
+			return err
+		}
+
+		// Send the chunk to the server
+        fmt.Println("About to write bytes to client")
+		_, err = conn.WriteToUDP(buffer[:n], addr)
+        fmt.Println("wrote bytes to client ", n)
+		if err != nil {
+			return err
+		}
+		if n < 65000 {
+            fmt.Println("chegou no final")
+            break
+        }
+		reply := make([]byte, 1024) 
+		nReply, _, err := conn.ReadFromUDP(reply);
+        if err != nil {
+            fmt.Println("Error reading server response: ", err)
+        }
+        fmt.Println(string(reply[:nReply]))
+		
+	}
+
+	return nil
+} 
+
+// Converting image.YCbCr format to one which we can manipulate the pixels
+func imgToTensor(img image.Image) [][]color.Color {
+	size := img.Bounds().Size()
+	var pixels [][]color.Color
+
+	for i := 0; i < size.X; i++ {
+		var y []color.Color
+		for j := 0; j < size.Y; j++ {
+			y = append(y, img.At(i, j))
+		}
+		pixels = append(pixels, y)
+	}
+	return pixels
+}
+
+func tensorToImg(pixels [][]color.Color) image.Image {
+	rect := image.Rect(0, 0, len(pixels), len(pixels[0]))
+	nImg := image.NewRGBA(rect)
+
+	for x := 0; x < len(pixels); x++ {
+		for y := 0; y < len(pixels[0]); y++ {
+			q := pixels[x]
+			if q == nil {
+				continue
+			}
+			p := pixels[x][y]
+			if p == nil {
+				continue
+			}
+			original, ok := color.RGBAModel.Convert(p).(color.RGBA)
+			if ok {
+				nImg.Set(x, y, original)
+			}
+		}
+	}
+
+	return nImg
+
+}
+
+func greyScale(pixels *[][]color.Color) {
+	ppixels := *pixels
+	xLen := len(ppixels)
+	yLen := len(ppixels[0])
+
+	numThreads := runtime.NumCPU()
+
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
+
+	processSection := func(startX, endX int) {
+		defer wg.Done()
+		for x := startX; x < endX; x++ {
+			for y := 0; y < yLen; y++ {
+				pixel := ppixels[x][y]
+				originalColor, ok := color.RGBAModel.Convert(pixel).(color.RGBA)
+				if !ok {
+					fmt.Println("type conversion went wrong")
+				}
+				grey := uint8(float64(originalColor.R)*0.21 + float64(originalColor.G)*0.72 + float64(originalColor.B)*0.07)
+				col := color.RGBA{
+					grey,
+					grey,
+					grey,
+					originalColor.A,
+				}
+				ppixels[x][y] = col
+			}
+		}
+	}
+
+	for i := 0; i < numThreads; i++ {
+		startX := (xLen * i) / numThreads
+		endX := (xLen * (i + 1)) / numThreads
+		go processSection(startX, endX)
+	}
+
+	wg.Wait()
+}
+
+func imageToBytes(img image.Image) ([]byte, error) {
+	var buf bytes.Buffer
+	err := png.Encode(&buf, img)
+	if err != nil {
+		return nil, err
+	}
+
+	// Converta para base64 ou use buf.Bytes() diretamente dependendo dos requisitos
+	return buf.Bytes(), nil
+}
+
