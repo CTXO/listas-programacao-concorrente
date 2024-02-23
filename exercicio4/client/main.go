d30b94e2c5aa7ac0bebd44e85e09d00519b63b5e
“arturlealbs” <albs@cin.ufpe.br>
2024-02-23 19:46:53 -0300

    added rpc/client and rpc/server

---
+++ exercicio4/client/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
+	"net/rpc"
+	"os"
+	"path/filepath"
+)
+
+type Args struct {
+	Image []byte
+}
+
+func main() {
+	//absolutePath, err := filepath.Abs("imgs/Apple.png")
+	absolutePath, err := filepath.Abs("imgs/Cake.png")
+	//absolutePath, err := filepath.Abs("imgs/Painting.png")
+	//absolutePath, err := filepath.Abs("imgs/Star.png")
+	if err != nil {
+		fmt.Println("Error getting absolute path: ", err)
+		return
+	}
+
+	//Getting the image from the client adress
+	img, err := openImage(absolutePath)
+	if err != nil {
+		fmt.Println("Error getting image: ", err)
+		return
+	}
+
+	//Converting the image to bytes so it can be sent
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, img)
+	if err != nil {
+		fmt.Println("Error encoding image: ", err)
+		return
+	}
+	imageBytes := buf.Bytes()
+
+	//Connecting with the server
+	client, err := rpc.DialHTTP("tcp", "localhost"+":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Requesting Greyscale Service
+	var reply []byte
+	args := Args{imageBytes}
+	err = client.Call("GreyImage.greyScaleServerRPC", args, &reply)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Saving Image received from Server
+	newImg, err := bytesToImg(reply)
+	if err != nil {
+		fmt.Println(err)
+	}
+	saveImage(newImg)
+}
+
+func openImage(path string) (image.Image, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+func bytesToImg(data []byte) (image.Image, error) {
+	img, _, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+func saveImage(img image.Image) error {
+	path, err := filepath.Abs("greyscale.png")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return err
+	}
+
+	fg, err := os.Create(path)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return err
+	}
+	defer fg.Close()
+	err = png.Encode(fg, img)
+	if err != nil {
+		fmt.Println("Error encoding file:", err)
+		return err
+	}
+
+	fmt.Println("Image saved")
+	return nil
+}
---
+++ exercicio4/server/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"net"
+	"net/http"
+	"net/rpc"
+	"runtime"
+	"sync"
+)
+
+type Args struct {
+	Image []byte
+}
+
+type GreyImage []byte
+
+func (g *GreyImage) greyScaleServerRPC(args *Args, reply *[]byte) error {
+	img, err := bytesToImg(args.Image)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pixels := imgToTensor(img)
+	greyScale(&pixels)
+	img = tensorToImg(pixels)
+
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, img)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	*reply = buf.Bytes()
+	return nil
+}
+
+func main() {
+	// Create a new RPC server
+	greyImage := new(GreyImage)
+	// Register RPC server
+	rpc.Register(greyImage)
+	rpc.HandleHTTP()
+	// Listen for requests on port 8080
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	http.Serve(l, nil)
+}
+
+func bytesToImg(data []byte) (image.Image, error) {
+	img, _, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+// Converting image.YCbCr format to one which we can manipulate the pixels
+func imgToTensor(img image.Image) [][]color.Color {
+	size := img.Bounds().Size()
+	var pixels [][]color.Color
+
+	for i := 0; i < size.X; i++ {
+		var y []color.Color
+		for j := 0; j < size.Y; j++ {
+			y = append(y, img.At(i, j))
+		}
+		pixels = append(pixels, y)
+	}
+	return pixels
+}
+
+func tensorToImg(pixels [][]color.Color) image.Image {
+	rect := image.Rect(0, 0, len(pixels), len(pixels[0]))
+	nImg := image.NewRGBA(rect)
+
+	for x := 0; x < len(pixels); x++ {
+		for y := 0; y < len(pixels[0]); y++ {
+			q := pixels[x]
+			if q == nil {
+				continue
+			}
+			p := pixels[x][y]
+			if p == nil {
+				continue
+			}
+			original, ok := color.RGBAModel.Convert(p).(color.RGBA)
+			if ok {
+				nImg.Set(x, y, original)
+			}
+		}
+	}
+
+	return nImg
+
+}
+
+func greyScale(pixels *[][]color.Color) {
+	ppixels := *pixels
+	xLen := len(ppixels)
+	yLen := len(ppixels[0])
+
+	numThreads := runtime.NumCPU()
+
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
+
+	processSection := func(startX, endX int) {
+		defer wg.Done()
+		for x := startX; x < endX; x++ {
+			for y := 0; y < yLen; y++ {
+				pixel := ppixels[x][y]
+				originalColor, ok := color.RGBAModel.Convert(pixel).(color.RGBA)
+				if !ok {
+					fmt.Println("type conversion went wrong")
+				}
+				grey := uint8(float64(originalColor.R)*0.21 + float64(originalColor.G)*0.72 + float64(originalColor.B)*0.07)
+				col := color.RGBA{
+					grey,
+					grey,
+					grey,
+					originalColor.A,
+				}
+				ppixels[x][y] = col
+			}
+		}
+	}
+
+	for i := 0; i < numThreads; i++ {
+		startX := (xLen * i) / numThreads
+		endX := (xLen * (i + 1)) / numThreads
+		go processSection(startX, endX)
+	}
+
+	wg.Wait()
+}
